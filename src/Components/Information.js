8b604db65616f6f3d717ab0d8febd30ff0d8641d
ericchee08 <ericchee@hotmail.co.uk>
2023-07-25 23:10:50 +0100

    update styling in reference to the images

--- src/Components/Information.js
+++ src/Components/Information.js
@@ -3,8 +3,8 @@ import iconSuccess from "./assets/images/icon-success.svg";
 const Information = () => {
   return (
     <div className="information">
-      <h1>Stay Updated!</h1>
-      <p>Join 60,000+ product managers receiving monthly updates on: </p>
+      <h1>Stay updated!</h1>
+      <p id="sub-heading">Join 60,000+ product managers receiving monthly updates on: </p>
       <p>
         <img className="icon-success" src={iconSuccess} alt="Success Icon" />
         Product discovery and building what matters
