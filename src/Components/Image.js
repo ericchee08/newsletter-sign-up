239edc397c022370aff3238b862618c36df984d0
Eric <110496410+EricChee8@users.noreply.github.com>
2023-07-26 19:48:43 +0100

    updating styling for mobile view

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,5 +1,19 @@
+import illustrationSignUpDesktop from "./assets/images/illustration-sign-up-desktop.svg"
+import illustrationSignUpMobile from "./assets/images/illustration-sign-up-mobile.svg"
+
 const Image = () => {
-  return <div className="image">Image</div>;
+  return <div className="image">
+  <img
+    className="illustration-signup-desktop"
+    src={illustrationSignUpDesktop}
+    alt=""
+  />
+  <img
+    className="illustration-signup-mobile"
+    src={illustrationSignUpMobile}
+    alt=""
+  />
+</div>
 };
 
 export default Image;
